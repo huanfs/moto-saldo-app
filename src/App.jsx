05888a7dee29bc35ffd4e2d60d972a3a9bbd40c9
Ruan Figuereido <huanfiguereido@gmail.com>
2024-09-27 18:16:44 -0300

    :computer: add / adicionei o contexto global

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { Globals } from "@context/context.jsx";
+
 import "./App.css";
 
 export function App(){
   return(
-    <p>site do zero</p>
+    <Globals>
+      <BrowserRouter>
+        <Routes>
+          oi
+        </Routes>
+      </BrowserRouter>
+    </Globals>
   )
 }
