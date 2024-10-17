834cc3a9d65cc14dafa6e759fe9f54624335ca2a
Ruan Figuereido <huanfiguereido@gmail.com>
2024-10-17 11:24:05 -0300

    :computer: develop / LogIn()  adicionei as mudanças e função para autenticação de usuário ( AINDA EM DESENVOLVIMENTO ) agora é possível realizar o logIn

--- src/routes/Enter.jsx
+++ src/routes/Enter.jsx
@@ -1,14 +1,42 @@
-import React from "react";
+import React, { useRef } from "react";
 
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom"; //* import navigate hook
 
+/* react-icons */
 import { FaArrowRight } from "react-icons/fa";
 import { FaUser } from "react-icons/fa";
 import { FaLock } from "react-icons/fa";
+/* react-icons */
 
-import style from "@styles/Enter.module.css";
+import style from "@styles/Enter.module.css"; //* stylesheet
 
 export default function Enter(){
+
+    const navigateTo = useNavigate(); // use navigate hook
+
+    const userName = useRef(null); // references to inputs
+    const password = useRef(null);
+
+    async function LogIn(){
+        const user = {
+            name: userName.current.value,
+            password: password.current.value,
+        };
+
+        try{
+            const searchUser = await fetch("http://localhost:8182/authenticate",{
+                method:"POST",
+                headers:{
+                    "Content-Type":"application/json",
+                },
+                body:JSON.stringify(user),
+                mode:"cors",
+            }).then((response)=>{response.status==200? navigateTo("/main"):null})
+        }
+        catch (err) {
+            console.log(err)
+        }
+    }
     return(
         <main className={style.container}>
             <h1>entrar</h1>
@@ -17,21 +45,40 @@ export default function Enter(){
                     <label htmlFor="user">usuário</label>
                     <div>
                         <FaUser/>
-                        <input type="text"/>
+                        <input type="text" ref={userName}/>
                     </div>
                 </div>
                 <div>
                     <label htmlFor="password">senha</label>
                     <div>
                         <FaLock/>
-                        <input type="text"/>
+                        <input type="text" ref={password}/>
                     </div>
                 </div>
             </form>
             <section>
-                <Link to="/config01"><button type="button">entrar<FaArrowRight/></button></Link>
+                <button type="button" onClick={LogIn}>entrar<FaArrowRight/></button>
                 <Link to="/">esqueci a minha senha</Link>
             </section>
         </main>
     )
 }
+
+// este componente de entrada é responsável pela autenticação do usuário
+// a função LogIn() é a responsável por armazenar os valores nos campos
+// de nome de usuario e senha através das suas respectivas referencias 
+//capturadas através do hook useRef do react.
+// então cria um objeto user que contém name e password recebendo os valores
+// das respectivas referências.
+// então executa um bloco try catch onde try faz uma requisição do tipo POST
+// para a rota /authenticate e envia o objeto user no corpo da requisição
+
+// a api recebe os valores e executa a consulta, se o usuário estiver no banco
+// retorna a mensagem de estatos 200 OK e então o hook useNavigate do react-router-dom
+// é executado direcionando imediatamente para a rota /main 
+
+ /*
+ são necessárias alterações com o direcionamento direto para MAIN onde se caso o usuário for novo
+ (que será descoberto realizando busca para as outras tabelas) decide direcionar para as rotas de
+ configuração ou não.
+ */
