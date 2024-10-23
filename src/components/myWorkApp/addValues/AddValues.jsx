645449b2d2cc7c49930a62f206f0685f1c185c16
Ruan Figuereido <huanfiguereido@gmail.com>
2024-10-23 10:03:44 -0300

    :tada: feat / o componente AddValues responsável por adicionar os valores de cada aplicativo armazena o valor dentro de um estado devidamente formatado com virgula para diferenciar os reais dos centavos

--- src/components/myWorkApp/addValues/AddValues.jsx
+++ src/components/myWorkApp/addValues/AddValues.jsx
@@ -1,18 +1,25 @@
-import React from "react";
+import React,{ useRef, useState } from "react";
 
 import style from "./AddValues.module.css";
 
 function AddValues({ close, appImage }){
+
+    const[value, setValue] = useState(0)
+
     return(
         <article className={style.new}>
             <img src={appImage}/>
 
-            <input type="text" placeholder="ganhos"/>
-            <input type="text" placeholder="gastos"/>
-            <input type="text" placeholder="distancia"/>
-            <input type="text" placeholder="horas"/>
+            <input 
+            type="text" 
+            placeholder="ganhos" 
+            onChange={((event)=>{
+                setValue(Intl.NumberFormat('pt-br').format(event.target.value.replace(/\D/g,'') / 100))
+            })}/>
+            <input type="text" placeholder="KM/percorridos"/>
+            <input type="text" placeholder="horas/minutos gastos"/>
 
-            <span>99,99</span>
+            <span>{value ? value : "00,00"}</span>
             <input 
             type="button" 
             value="adicionar"
