d739762a950a0f8afb3824538dd60ed525968683
Ruan Figuereido <huanfiguereido@gmail.com>
2024-11-03 10:01:31 -0300

    :tada: feat / Details.jsx agora executa funcionalidades de calcular os valores de total, distancia e tempo e exibir estes totais a partir de userData no menuDropDown

--- src/components/header/details/Details.jsx
+++ src/components/header/details/Details.jsx
@@ -1,21 +1,61 @@
-import React from "react";
+import React, { useContext, useState, useEffect } from "react";
+
+import { Context } from "@context/context.jsx"; // importação do contexto
 
 import style from "./Details.module.css";
 
 function Details(){
+
+    const { userData } = useContext( Context ) // utilização do contexto
+
+    const[details, setDetails] = useState({
+        "total":0,
+        "distance":0,
+        "time":0
+    })
+
+    useEffect(() => {
+        if (userData && typeof userData === "object") {
+            let total = 0;  //variáveis que vão armazenar cada valor individualmente
+            let distance = 0;
+            let time = 0;
+
+            userData.apps.forEach((item) => { // o loop mapeia cada propriedade correspondente e insere o valor 
+                total += item.total;          // dentro da variável.
+                distance += item.distance;
+                time += item.time;
+            });
+            // Atualiza o estado com os valores acumulados de cada variável
+            setDetails({ total, distance, time });
+        }
+    }, [userData]);
+
     return(
         <article className={style.details}>
             <div>
                 <p>ganhos totais</p>
-                <span>00,00</span>
+                <span>
+                    {
+                        details.total != 0 ? details.total : "00,00"
+                    }
+                    </span>
             </div>
             <div>
                 <p>KM/s percorridos</p>
-                <span>00,00</span>
+                <span>
+                    {
+                        details.distance != 0 ? details.distance : "00.0"
+                    }
+
+                </span>
             </div>
             <div>
                 <p>horas trabalhadas</p>
-                <span>00,00</span>
+                <span>
+                    {
+                        details.time != 0 ? details.time : "00:00"
+                    }
+                    </span>
             </div>
         </article>
     )
