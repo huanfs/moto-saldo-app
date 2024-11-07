0e1f0558036750505611bda36e5ef2a7dcf8f3e4
Ruan Figuereido <huanfiguereido@gmail.com>
2024-11-07 14:26:20 -0300

    :computer: refactor / comentei o funcionamento da função de adicionar aplicativos e usei a propriedade name diretamente na crianção do objeto salvo dentro de apps

--- src/components/workApp/WorkApp.jsx
+++ src/components/workApp/WorkApp.jsx
@@ -1,34 +1,30 @@
 import React,{ useContext } from "react";
 
-import { Context } from "@context/context.jsx";  //* import context
+import { Context } from "@context/context.jsx";
 
-import style from "./WorkApp.module.css"; // stylesheet
+import style from "./WorkApp.module.css";
 
 export default function WorkApp({logotype, name}){
 
-    const { setUserConfig } = useContext(Context);  //* using context
-
-    function AddApp(event){
-        const NewApp = event.target.parentElement.id;
+    const { userConfig,setUserConfig } = useContext(Context);
+    /*
+    CRIA NO ESTADO 'userConfig.apps' UM OBJETO
+     
+    */
+   React.useEffect(()=>{
+    console.log(userConfig.apps)
+   },[userConfig])
+    function AddApp(){
+        // const NewApp = name;
         setUserConfig((prevValue)=>({
-            ...prevValue, apps:[...prevValue.apps, {"appName":NewApp, "total":0, "distance":0, "time":0}]
+            ...prevValue, apps:[...prevValue.apps, {"appName":name, "total":0, "distance":0, "time":0}]
         }))
     }
 
     return(
-        <div className={style.application} id={name} onClick={AddApp}>
+        <div className={style.application} onClick={AddApp}>
             <img src={logotype} alt={name}/>
             <span>{name}</span>
         </div>
     )
 }
-
-/*  
-    this component import the atualization function 'setUserConfig'
-    from context.
-    this component receive 'LOGOTYPE' and 'NAME' from props.
-    the function AddApp catches the id of div element, (ID = 'NAME') and storages into
-    'setUserConfig'.
-    finally, render and DIV element and an IMG and SPAN element
-    as child.
-*/
