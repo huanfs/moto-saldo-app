d765c2ff7f29c0984a0153cc85836f3b2238481b
huanfs <huanfiguereido@gmail.com>
2025-09-11 03:10:30 -0300

    :bug: fix: cria pasta public com imagens que agora serão acessadas por lá

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -5,11 +5,11 @@ export const Context = createContext();
 export const Globals = ({ children }) => {
     
     const logo = [
-        {"logotype":"src/assets/images/uber-logo.png", "name":"uber"},
-        {"logotype":"src/assets/images/99-logo.png", "name":"99"},
-        {"logotype":"src/assets/images/ifood-logo.png", "name":"ifood"},
-        {"logotype":"src/assets/images/pedeai-logo.png", "name":"pede ai"},
-        {"logotype":"src/assets/images/mercadoenvios-logo.png", "name":"mercado en."},
+        {"logotype":"/images/uber-logo.png", "name":"uber"},
+        {"logotype":"/images/99-logo.png", "name":"99"},
+        {"logotype":"/images/ifood-logo.png", "name":"ifood"},
+        {"logotype":"/images/pedeai-logo.png", "name":"pede ai"},
+        {"logotype":"/images/mercadoenvios-logo.png", "name":"mercado en."},
     ];
 
     /*
